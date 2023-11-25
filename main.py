6f47c426dd78c6c76baeef9a3261a12185b7061f
Korky <jakubhosek2009@gmail.com>
2023-11-25 11:51:38 +0100

    pridaní exit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import sys
 
 pygame.init()
 
@@ -8,10 +9,16 @@ screen_height = 600
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Fiktivní firma")
 
+
 run = True
 while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False 
+        elif event.type == pygame.KEYDOWN:
+            # Zpracování stisknutí klávesy
+            if event.key == pygame.K_ESCAPE:
+                pygame.quit()
+                sys.exit()
 
 pygame.quit()
