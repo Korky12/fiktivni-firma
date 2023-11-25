7552f15a1dfebda52ec4652ea778afc3163f3dfc
Korky <jakubhosek2009@gmail.com>
2023-11-25 12:15:11 +0100

    FPS

--- main.py
+++ main.py
@@ -9,16 +9,31 @@ screen_height = 600
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Fiktivní firma")
 
+FPS = 60
 
-run = True
-while run:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False 
-        elif event.type == pygame.KEYDOWN:
-            # Zpracování stisknutí klávesy
-            if event.key == pygame.K_ESCAPE:
-                pygame.quit()
-                sys.exit()
+DARK_BLUE = (52, 90, 115)
 
-pygame.quit()
+def draw_window():
+    screen.fill(DARK_BLUE)
+    pygame.display.update()
+
+def main():
+    clock = pygame.time.Clock()
+    run = True
+    while run:
+        clock.tick(FPS)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False 
+            elif event.type == pygame.KEYDOWN:
+                # Zpracování stisknutí klávesy
+                if event.key == pygame.K_ESCAPE:
+                    pygame.quit()
+                    sys.exit()
+        draw_window()
+    
+    
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
